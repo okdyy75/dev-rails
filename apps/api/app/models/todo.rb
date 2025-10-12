c976c83a4634ce6363fec76c3f058726d7378909
okdyy75 <okdyy75@gmail.com>
2025-10-13 01:08:54 +0900

    commentモデル追加

--- apps/api/app/models/todo.rb
+++ apps/api/app/models/todo.rb
@@ -1,3 +1,5 @@
 class Todo < ApplicationRecord
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true, length: { maximum: 255 }
 end
--- apps/api/db/schema.rb
+++ apps/api/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_19_000000) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_12_144138) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "todo_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_id"], name: "index_comments_on_todo_id"
+  end
+
   create_table "todos", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
@@ -18,4 +26,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_19_000000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "comments", "todos"
 end
